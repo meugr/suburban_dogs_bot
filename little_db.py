c2cace49df8bed82ee8aa5cecf04d7f83f125b05
Meugr <meugr32@gmail.com>
2019-07-01 17:09:25 +0300

    Add MongoDB, create create_new_user, get_branch and set_branch methods in class UserData remove redis support

--- little_db.py
+++ little_db.py
@@ -1,47 +1,81 @@
 import config
-import redis
+from rejson import Client, Path
+import pymongo
 import json
+from collections import deque
 
 
 class UserData:
-    """Класс для работы с пользовательскими данными из redis"""
+    """
+    Класс для работы с пользовательскими данными из mongoDB
+    """
+
     def __init__(self):
-        self.conn = redis.Redis(db=1, charset='utf-8', decode_responses=True)
-    def new_user():
-        pass
+        self.db = pymongo.MongoClient().suburban_dogs.user_data
 
+    def create_new_user(self, chat_id):
+        """Создаем нового юзера в mongoDB"""
+        self.db.insert_one({
+            'id': chat_id,
+            'state': 'home',  # (home, search, settings, history)
+            'search': {},
+            'settings': {},
+            'history': ()})
 
-class UserState(UserData):
-    '''
-    Функции для работы с redis и управлением состоянием пользователя (FSM)
-    '''
-    def start_search(self, chat_id):
-        '''Создает в redis запись, тем самым начиная опрос юзера'''
-        self.conn.delete(chat_id)
-        self.conn.hmset(chat_id, {'search': 'True'})
+    def get_branch(self, chat_id, branch):
+        return self.db.find_one({'id': chat_id})[branch]
 
-    def reset_info(self, chat_id):
+    def set_branch(self, chat_id, branch, changes):
+        """Отправляет изменения ветки в mongoDB"""
+        self.db.update_one(
+            {'id': chat_id},
+            {'$set': {branch: changes}})
+
+    def change_current_branch(self, chat_id, branch):
+        """Задаем текущую ветку в mongoDB
+        maybe unused"""
+        self.db.update_one({'id': chat_id}, {'$set': {'state': branch}})
+
+    def reset_search_info(self, chat_id):
         '''Отмена опроса юзера и удаление записи о начале поиска из redis'''
-        self.conn.delete(chat_id)
+        self.conn.jsonset(chat_id, Path(f'.{chat_id}.search'), {})
+        self.change_current_branch(chat_id, 'home')
+
+    def get_search_info(self, chat_id):
+        '''Получаем весь юзерввод из redis'''
+        return self.conn.jsonget(chat_id, Path(f'.{chat_id}.search'))
+
+
+
+
+
+
+
+
+
+    # def start_search(self, chat_id):
+    #     '''Создает в redis запись, тем самым начиная опрос юзера'''
+    #     self.conn.delete(chat_id)
+    #     # self.conn.hmset(chat_id, {'search': 'True'})
+
 
     def append_info(self, chat_id, step, user_input):
         '''Добавление в redis пользовательского ввода'''
         return self.conn.hset(chat_id, step, user_input)
 
-    def get_info(self, chat_id):
-        '''Получаем весь юзерввод из redis'''
-        return self.conn.hgetall(chat_id)
 
-
-class UserSettings():
+class UserSettings(UserData):
     pass
 
 
-class UserHistory():
+class UserHistory(UserData):
     pass
 
 
 class StationInfo:
+    """
+    Класс для работы с json базой электричек
+    """
     def __init__(self, path_to_db):
         with open(path_to_db) as f:
             self.db = json.load(f)
